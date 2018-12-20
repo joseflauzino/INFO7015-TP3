6f26a170ee201920a0228d7fd1158d4da3f7cc42
jwvflauzino <jose.wilson.vf@gmail.com>
2018-12-20 13:29:39 -0200

    Limpando Repositorio

--- config_firefox.sh
+++
@@ -1,3 +0,0 @@
-mv firefox /opt/
-mv /usr/bin/firefox /usr/bin/firefox_old2
-ln -s /opt/firefox/firefox /usr/bin/firefox
--- set_ryu_router.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# setting IP addresses
-curl -X POST -d '{"address":"192.168.1.254/24"}' http://localhost:8080/router/0000000000000001
-curl -X POST -d '{"address":"192.168.2.254/24"}' http://localhost:8080/router/0000000000000001
-curl -X POST -d '{"address":"192.168.3.254/24"}' http://localhost:8080/router/0000000000000001
