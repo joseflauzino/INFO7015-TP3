bf4c1604143a808950bd81094269f60553f8fabe
jwvflauzino <jose.wilson.vf@gmail.com>
2018-12-20 23:03:39 -0200

    Versão Final

--- generate_result/make_plot.py
+++ generate_result/make_plot.py
@@ -19,21 +19,22 @@ def make_plot(data):
 
     axes = plot.subplot(111)
     w = 0.5
-    axes.bar(1, 22.54, width=w, edgecolor='White', color='lightskyblue',label="Tempo de Referencia",align="center")
-    axes.bar(2, data['scenario1']['duration'], width=w, edgecolor='White', color='cadetblue',label="Com VNF (Chrome)", align="center")
-    axes.bar(3, data['scenario2']['duration'], width=w, edgecolor='White', color='lightseagreen',label="Sem VNF (Chrome)", align="center")
-    axes.bar(4, data['scenario3']['duration'], width=w, edgecolor='White', color='teal',label="Com VNF (Firefox)",align="center")
-    axes.bar(5, data['scenario4']['duration'], width=w, edgecolor='White', color='darkslategrey',label="Sem VNF (Firefox)", align="center")
+    axes.bar(1, 22.54, width=w, edgecolor='White', color='darkgray',label="Tempo de Referencia",align="center")
+    axes.bar(2, data['scenario1']['duration'], width=w, edgecolor='White', color='lightblue',label="Com VNF (Chrome)", align="center")
+    axes.bar(3, data['scenario2']['duration'], width=w, edgecolor='White', color='cadetblue',label="Sem VNF (Chrome)", align="center")
+    axes.bar(4, data['scenario3']['duration'], width=w, edgecolor='White', color='mediumseagreen',label="Com VNF (Firefox)",align="center")
+    axes.bar(5, data['scenario4']['duration'], width=w, edgecolor='White', color='seagreen',label="Sem VNF (Firefox)", align="center")
     for i in range(len(x)):
-        axes.text(x[i] - 0.2, durations[i] + 1, truncate(durations[i], 2), fontsize=11, color='darkslategrey')
+        if i == 2 or i == 4:
+            axes.text(x[i] - 0.25, durations[i] + 1, truncate(durations[i], 2), fontsize=12, color='darkslategrey')
+        else:
+            axes.text(x[i] - 0.2, durations[i] + 1, truncate(durations[i], 2), fontsize=12, color='darkslategrey')
 	
-    axes.set_xlabel("Cenarios")
+    axes.set_xlabel("Cenarios",fontsize=12)
     axes.tick_params(labelbottom=False)
     plot.axis((0,6,0,165))
-    axes.set_ylabel("Tempo (em segundos)")
+    axes.set_ylabel("Tempo (em segundos)",fontsize=12)
     axes.legend(loc=9,bbox_to_anchor=(0.5,-0.06),ncol=2,fontsize=12)
-    #axes.legend(loc="upper right", bbox_to_anchor=(0.5, -0.1), ncol=2)
-    # left, bottom, right, top
     plot.tight_layout(rect=[0, 0.15, 1, 1])
     plot.savefig("fig3.png")
 
